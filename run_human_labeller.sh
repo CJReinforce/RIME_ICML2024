f88335b05444efc0c011d6d0eb8d91e90b1dfabc
CJReinforce <chengjie2022@ia.ac.cn>
2024-06-28 19:51:23 +0800

    update scripts

--- run_human_labeller.sh
+++ run_human_labeller.sh
@@ -15,26 +15,17 @@ run_command() {
 
     # PEBBLE
     if [ "$algorithm" == "pebble" ]; then
-        python train_PEBBLE_with_actual_human_labeller.py --device=$device --env="$envname" --seed="$seed" --actor_lr=$sac_lr --critic_lr=$sac_lr --unsup_steps=$unsup_steps --steps=1000000 --num_interact=$num_interact --max_feedback="$feedback" --reward_batch="$reward_batch" --reward_update=50
+        python train_PEBBLE_with_actual_human_labeller.py --device=$device --env="$envname" --seed="$seed" --actor_lr=$sac_lr --critic_lr=$sac_lr --unsup_steps=$unsup_steps --steps=10000000 --num_interact=$num_interact --max_feedback="$feedback" --reward_batch="$reward_batch" --reward_update=50
         
     # RIME
     else
-        python train_RIME_with_actual_human_labeller.py --env="$envname" --seed="$seed" --actor_lr=$sac_lr --critic_lr=$sac_lr --unsup_steps=$unsup_steps --steps=1000000 --num_interact=$num_interact --max_feedback="$feedback" --reward_batch=$reward_batch --reward_update=50 --device="$device" --least_reward_update=30 --threshold_variance='kl' --threshold_alpha=0.5 --threshold_beta_init=3.0 --threshold_beta_min=1.0
+        python train_RIME_with_actual_human_labeller.py --env="$envname" --seed="$seed" --actor_lr=$sac_lr --critic_lr=$sac_lr --unsup_steps=$unsup_steps --steps=10000000 --num_interact=$num_interact --max_feedback="$feedback" --reward_batch=$reward_batch --reward_update=50 --device="$device" --least_reward_update=30 --threshold_variance='kl' --threshold_alpha=0.5 --threshold_beta_init=3.0 --threshold_beta_min=1.0
         
     fi
 }
 
 declare -A gpu_ids=(
     [12345]=0
-    [23451]=1
-    [34512]=2
-    [45123]=3
-    [51234]=4
-    [67890]=0
-    [68790]=1
-    [78906]=2
-    [89067]=3
-    [90678]=4
 )
 
 seeds=(12345)
